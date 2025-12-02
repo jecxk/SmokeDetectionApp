cac9e28263554f2adaea44abbcae5c3a3f4bf034
DemonLonely <tranxuanbach2004@gmail.com>
2025-12-02 19:19:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/smokedetection/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.smokedetection;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btnUploadImage;
+    Button btnUploadVideo;
+
+    ImageView previewImage;
+    private ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    if (data != null){
+                        Uri selectedUri = data.getData();
+                        // TODO: use the selected image / video URI
+                        if(selectedUri != null){
+                            previewImage.setImageURI(selectedUri);
+                        }
+                    }
+                }
+            }
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        previewImage = findViewById(R.id.previewImage);
+
+        // setup buttons
+        btnUploadImage = findViewById(R.id.btnUploadImage);
+        btnUploadVideo = findViewById(R.id.btnUploadVideo);
+
+        // function of button
+        btnUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery("image/*");
+            }
+        });
+
+        btnUploadVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery("video/*");
+            }
+        });
+    }
+
+    // access gallery
+    private void openGallery(String fileType) {
+        // ask user to choose files
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        // set type of file that able to choose
+        intent.setType(fileType);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        Intent chooser = Intent.createChooser(intent, "Select Files");
+        galleryLauncher.launch(chooser);
+    }
+}
